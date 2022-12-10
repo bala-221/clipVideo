56c8938a7c7e7f75f29f533583a950f2152159e9
Abubakar Bala <47645835+bala-221@users.noreply.github.com>
2022-12-10 16:26:37 +0800

    Update videoEditor.py

--- videoEditor.py
+++ videoEditor.py
@@ -1,6 +1,10 @@
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 𝐬𝐭𝐚𝐫𝐭_𝐭𝐢𝐦𝐞 = 5
 𝐞𝐧𝐝_𝐭𝐢𝐦𝐞 = 1845
-𝐟𝐟𝐦𝐩𝐞𝐠_𝐞𝐱𝐭𝐫𝐚𝐜𝐭_𝐬𝐮𝐛𝐜𝐥𝐢𝐩("tsokaci.mp4", 𝐬𝐭𝐚𝐫𝐭_𝐭𝐢𝐦𝐞, 𝐞𝐧𝐝_𝐭𝐢𝐦𝐞, 𝐭𝐚𝐫𝐠𝐞𝐭𝐧𝐚𝐦𝐞="tsokaci_cut.mp4")
 
-Habu = 1
+
+videoFile = '' #Please add the video file here
+
+
+𝐟𝐟𝐦𝐩𝐞𝐠_𝐞𝐱𝐭𝐫𝐚𝐜𝐭_𝐬𝐮𝐛𝐜𝐥𝐢𝐩("tsokaci.mp4", 𝐬𝐭𝐚𝐫𝐭_𝐭𝐢𝐦𝐞, 𝐞𝐧𝐝_𝐭𝐢𝐦𝐞, 𝐭𝐚𝐫𝐠𝐞𝐭𝐧𝐚𝐦𝐞=videoFile)
+
